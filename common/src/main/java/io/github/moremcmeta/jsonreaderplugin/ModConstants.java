22bc01e4cde65f6592d90e932e5c371445cd7756
soir20 <71418127+soir20@users.noreply.github.com>
2022-08-22 00:22:09 -0400

    Add comments + copyright notices

--- common/src/main/java/io/github/moremcmeta/jsonreaderplugin/ModConstants.java
+++ common/src/main/java/io/github/moremcmeta/jsonreaderplugin/ModConstants.java
@@ -1,7 +1,28 @@
+/*
+ * JSON Reader Plugin is a plugin for MoreMcmeta that reads JSON metadata files.
+ * Copyright (C) 2022 soir20
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.moremcmeta.jsonreaderplugin;
 
 import io.github.soir20.moremcmeta.api.client.metadata.MetadataReader;
 
+/**
+ * Constants for both Fabric and Forge implementations of the plugin.
+ * @author soir20
+ */
 public class ModConstants {
     public static final String MOD_ID = "moremcmeta_json_reader_plugin";
     public static final String DISPLAY_NAME = "MoreMcmeta JSON Metadata Reader";
--- fabric/src/main/java/io/github/moremcmeta/jsonreaderplugin/fabric/JsonReaderPluginFabric.java
+++ fabric/src/main/java/io/github/moremcmeta/jsonreaderplugin/fabric/JsonReaderPluginFabric.java
@@ -1,9 +1,30 @@
+/*
+ * JSON Reader Plugin is a plugin for MoreMcmeta that reads JSON metadata files.
+ * Copyright (C) 2022 soir20
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.moremcmeta.jsonreaderplugin.fabric;
 
 import io.github.moremcmeta.jsonreaderplugin.ModConstants;
 import io.github.soir20.moremcmeta.api.client.MoreMcmetaMetadataReaderPlugin;
 import io.github.soir20.moremcmeta.api.client.metadata.MetadataReader;
 
+/**
+ * Implementation of the JSON reader plugin on Fabric.
+ * @author soir20
+ */
 public class JsonReaderPluginFabric implements MoreMcmetaMetadataReaderPlugin {
 
     @Override
